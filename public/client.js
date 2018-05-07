bbd68c8489a6faa154fc3471f4e11337f31de0a0
Glitch (water-lan) <none>
2018-05-07 02:33:07 +0000

    🍜🚖 Checkpoint ./public/client.js:518198/2014 ./views/index.html:518198/189

--- public/client.js
+++ public/client.js
@@ -2,6 +2,10 @@
 
 var dartEvent = "";
 var dartDate = "";
+var dartName = "";
+var dartLocale = "";
+var dartDescription = "";
+var dartLink = "";
 var formattedDate = "";
 var currentTime = new Date();
 
@@ -17,10 +21,30 @@ function checkDates(dartDate){
 function writeEventsData(){
   $.getJSON('/events', function(data){
     $.each(data, function(key,value){
+      //Write Event Date!!
       dartDate = value[1].value;
       if(checkDates(dartDate)){
         $("#testDate").append("<li>" + dartDate + "</li>")
-      }
+        
+      //Write Event Name!! 
+      dartName = value[2].value;
+      console.log(dartName);
+      $("#testName").append("<li>" + dartName + "</li>")
+        
+      //Write Event Locale!! 
+      dartLocale = value[3].value;
+      console.log(dartLocale);
+      $("#testLocale").append("<li>" + dartLocale + "</li>")
+        
+      //Write Event Description!! 
+      dartDescription = value[4].value;
+      console.log(dartDescription);
+      $("#testDescription").append("<li>" + dartDescription + "</li>")
+      }//End CheckDates
     })})}
 
 writeEventsData();
+
+/* To Do 
+- Understand how laggy this is, and what the lag is exactly!! Seems to be slow to update from GSheets.
+*/

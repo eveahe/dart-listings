3d1b8ed2b29a4d6a34de1380374ed45c72ade1ce
Glitch (water-lan) <none>
2018-05-07 02:23:03 +0000

    🔋🙌 Checkpoint ./public/client.js:518198/886

--- public/client.js
+++ public/client.js
@@ -2,18 +2,25 @@
 
 var dartEvent = "";
 var dartDate = "";
+var formattedDate = "";
+var currentTime = new Date();
 
+//Writing an event date only if it is in the future
 function checkDates(dartDate){
   if(dartDate != "Date"){
-    Date(dartDate);
-  }
-}
+    formattedDate = new Date(dartDate);
+    if(formattedDate > currentTime){
+    return true;
+  }}}
 
+//Writing all Event Data...currently though it's just writing event dates... 
 function writeEventsData(){
   $.getJSON('/events', function(data){
     $.each(data, function(key,value){
       dartDate = value[1].value;
-      $("#testDate").append("<li>" + dartDate + "</li>")
+      if(checkDates(dartDate)){
+        $("#testDate").append("<li>" + dartDate + "</li>")
+      }
     })})}
 
 writeEventsData();
